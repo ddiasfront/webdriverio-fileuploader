79ae6cbdd38b191d1671c932610455fbdd5de78e
ddiasfront <ddiasfront@gmail.com>
2017-08-18 11:49:41 -0300

    segurando o tchan

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,59 @@
-function postFiles(file) {
-	$.ajax({
-	  url: "/file-uploader",
-	  type: "POST",
-	  data: {id: 'wtf'},
-	  dataType: "text/html"
-	});
-}
+$("#dropZoo").dropzone({ url: "/upload" });
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+// function postFiles(file) {
+// 	$.ajax({
+// 	  url: "/file-uploader",
+// 	  type: "POST",
+// 	  data: {id: 'wtf'},
+// 	  dataType: "text/html"
+// 	});
+// }
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    // ev.target.appendChild(document.getElementById(data));
-}
+// function allowDrop(ev) {
+//     ev.preventDefault();
+// }
 
-var dropArea = document.getElementById('dropArea');
+// function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     // ev.target.appendChild(document.getElementById(data));
+// }
 
-function traverseFileTree(item, path) {
-  path = path || "";
-  if (item.isFile) {
-    // Get file
-    console.log(item);
-    item.file(function(file) {
-      // console.log("File:", path + file.name, file);
-    });
-  } else if (item.isDirectory) {
-    // Get folder contents
-    // SE FOR O DIRETORIO POSSO CRIAR UM READER NELE
-    var dirReader = item.createReader();
-    // A PARTIR DO READER CRIADO LER AS ENTRADAS DESSE DIRETORIO
-    dirReader.readEntries(function(entries) {
-       // PARA CADA ENTRADITA TU TERAS UM LUPE
-      for (var i=0; i<entries.length; i++) {
-      	// A ENTRADA EH ESSA AQUI
-      	// DEPOIS QUE VOCE LE O DIRETORIO TODO VOCE PEGA CADA ITEM E USA A FUNCAO DE TRAVERSE PRA ITEM NO CASO AQUI
-        traverseFileTree(entries[i], path + item.name + "/");
-      }
-    });
-  }
-}
+// var dropArea = document.getElementById('dropArea');
 
-dropArea.addEventListener("drop", function(event) {
-  event.preventDefault();
-  var data = event.dataTransfer.getData("text");
-  var items = event.dataTransfer.items;
-  for (var i=0; i<items.length; i++) {
-    // webkitGetAsEntry is where the magic happens
-    var item = items[i].webkitGetAsEntry();
-    if (item) {
-      traverseFileTree(item);
-    }
-  }
-}, false);
+// function traverseFileTree(item, path) {
+//   path = path || "";
+//   if (item.isFile) {
+//     // Get file
+//     console.log(item);
+//     item.file(function(file) {
+//       // console.log("File:", path + file.name, file);
+//     });
+//   } else if (item.isDirectory) {
+//     // Get folder contents
+//     // SE FOR O DIRETORIO POSSO CRIAR UM READER NELE
+//     var dirReader = item.createReader();
+//     // A PARTIR DO READER CRIADO LER AS ENTRADAS DESSE DIRETORIO
+//     dirReader.readEntries(function(entries) {
+//        // PARA CADA ENTRADITA TU TERAS UM LUPE
+//       for (var i=0; i<entries.length; i++) {
+//       	// A ENTRADA EH ESSA AQUI
+//       	// DEPOIS QUE VOCE LE O DIRETORIO TODO VOCE PEGA CADA ITEM E USA A FUNCAO DE TRAVERSE PRA ITEM NO CASO AQUI
+//         traverseFileTree(entries[i], path + item.name + "/");
+//       }
+//     });
+//   }
+// }
+
+// dropArea.addEventListener("drop", function(event) {
+//   event.preventDefault();
+//   var data = event.dataTransfer.getData("text");
+//   var items = event.dataTransfer.items;
+//   for (var i=0; i<items.length; i++) {
+//     // webkitGetAsEntry is where the magic happens
+//     var item = items[i].webkitGetAsEntry();
+//     if (item) {
+//       traverseFileTree(item);
+//     }
+//   }
+// }, false);
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 var path = require('path');
+const fileUpload = require('express-fileupload');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(fileUpload());
 app.set('view engine', 'hjs');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -20,19 +19,26 @@ app.get('/', function(req, res){
   });
 });
 
-app.post('/test-page', function(req, res) {
-    var name = req.body.name,
-        city = req.body.city;
-    console.log(name, city);
-    // ...
+app.post('/upload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+ 
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
+  let sampleFile = req.files.sampleFile;
+
+  console.log(req.files.file);
+  // console.log(sampleFile);
+
+  // Use the mv() method to place the file somewhere on your server 
+  req.files.file.mv('./' + req.files.file.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+ 
+    res.send('File uploaded!');
+  });
 });
 
-app.post('/file-uploader', function(req, res, next) {
-    console.log(req.body);
-    console.log(req.files);
-});
-
-http.listen(3000, function(){
+app.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
